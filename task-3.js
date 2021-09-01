18133e2b934987e37429e4af6846087218eb79ea
Mehedi390 <mh0816150@gmail.com>
2021-08-31 17:59:34 -0700

    task 4 complate

--- task-3.js
+++ task-3.js
@@ -1,2 +1,20 @@
 const number = () => 89
 console.log(number());
+// 3.2
+const divied = num => num / 7;
+console.log(divied(70));
+// 3.3
+const math = (num1, num2) => {
+    const add = num1 + num2;
+    const result = add / 2;
+    return result;
+}
+console.log(math(10, 2));
+// 3.4
+const mathOperation = (a, b) => {
+    const num1 = a + 7;
+    const num2 = b + 7;
+    const result = num1 + num2;
+    return result;
+}
+console.log(mathOperation(10, 4));
---
+++ task-4.js
@@ -0,0 +1,8 @@
+const numbers = [241, 5, 215, 22, 154, 21, 21, 21];
+const newNumbers = numbers.map(divied);
+function divied(num) {
+    const result = num / 7;
+    return result
+}
+console.log(newNumbers);
+
